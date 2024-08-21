2afd1b923fcdd82963c912d90ed82285f111463f
lokeshkumar <lokeshvenkatesan18@gmail.com>
2024-08-21 12:27:22 +0530

    file updated

--- otp.java
+++ otp.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class otp {
     public static void main(String[] args) throws Exception {
         Scanner sc=new Scanner(System.in);     
-        
+        while(true){
         int opt=(int)(Math.random()*99999+99999);
         System.out.println("\t\t"+opt);
         System.out.println("\t\tOTP GENERATED ... ");
@@ -16,8 +16,9 @@ public class otp {
         else{
             System.out.println("\t\tINVALID OTP !?");
         }
-        
-        
+        System.out.println();
+        Thread.sleep(1000); 
+    }
 
     }
 
