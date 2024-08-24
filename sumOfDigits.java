59018287926f574a8cf8ded99e62b05e02d91c81
lokeshkumar <lokeshvenkatesan18@gmail.com>
2024-08-24 15:45:34 +0530

    filesadded

--- sumOfDigits.java
+++ sumOfDigits.java
@@ -6,17 +6,20 @@ public class sumOfDigits{
         System.out.print("ENTER THE NUMBER : ");
         int rem=0; 
         int sum=0;
+        int product=0;
         int num=sc.nextInt();
         while(num>0 )
         {
             
-            rem=num % 10; //  => REM = NUM % 10
-            sum+=rem;     //  => SUM = SUM + REM
+            rem=num % 10; //  => REM = NUM % 10             |_ WE CAN ALSO GIVE => SUM + = NUM % 10;
+            sum+=rem;     //  => SUM = SUM + REM 
+            product*=rem;           |
             num/= 10;       // => NUM = NUM / 10
            
                        
         } 
         System.out.println("THE SUM OF THE NUMBER IS :"+sum);
+        System.out.println("THE PRODUCT OF THE NUMBER IS :"+product);
     }
     
 }
